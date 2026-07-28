04217457e416fea1bdba45340fe1e7e464512cc6
Mahyas <MahyasGolparian@gmail.com>
2026-07-28 13:34:12 -0800

    Create user.go

---
+++ internal/model/user.go
@@ -0,0 +1,10 @@
+package model
+
+import "time"
+
+type User struct {
+	ID           int       `db:"id"`
+	Username     string    `db:"username"`
+	PasswordHash string    `db:"password_hash"`
+	CreatedAt    time.Time `db:"created_at"`
+}
